757a62d98adfc97879647c03a5304b968d0da959
TDPrograms <fieldsontori@gmail.com>
2022-10-26 14:37:03 -0700

    First commit--added a main.py(blank) file and animal.py(with a class and two methods)

---
+++ animal.py
@@ -0,0 +1,12 @@
+class Animal():
+    def __init__(self):
+        self.type = ""
+        self.size = 0
+        self.age = 0
+        self.name = ""
+    
+    def speak(self):
+        pass
+
+    def describe(self):
+        pass
